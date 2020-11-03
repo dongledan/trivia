43af4af43c6a39e7526db317a97dfb66bb1004ab
Danny Li <dongledan@yahoo.com>
2020-11-03 11:18:54 -0500

    fixed safari issues & misc

--- src/components/Questions/NextGame.js
+++ src/components/Questions/NextGame.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NextGame(props) {
   const {setPlayingTrivia} = props;
   return (
-  <div style={{marginTop: '100px', width: '100%', textAlign: 'right'}}>
+  <div style={{marginTop: '100px', marginBottom: '20px', width: '95%', textAlign: 'right'}}>
     <button className='button' style={{background: '#ddd', opacity: '0.7'}} onClick={() => setPlayingTrivia(false)}>Exit</button>
   </div>
   )
